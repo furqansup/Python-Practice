24a38078684ac099f303fda97446bdf697b72e19
furqansup <furqanahmedmj@gmail.com>
2026-06-27 23:37:59 +0530

    practice

---
+++ DSA/linearSearch.py
@@ -0,0 +1,67 @@
+# Linear Search
+
+# Find whether 15 exists in the list.
+
+
+
+# def linearSearch(arr, target):
+#     for i in range(len(arr)):
+#         if arr[i] == target:
+#             return f'the target {target} is in {i} index'
+#     return -1
+        
+# ar = [3, 7, 10, 15, 20]
+# tar = 15
+
+# print(linearSearch(ar, tar))
+
+
+# Find 50 in the list
+
+# def search(arr, target):
+#     for i in range(len(arr)):
+#         if arr[i] == target:
+#             return f'the target {target} is in {i} index'
+#     else: 
+#         return 'Not found'
+
+# a =  [12, 25, 31, 44, 55]
+# t = 50
+
+# print(search(a, t))
+
+
+# Return the index of the first occurrence.
+
+# def occurence(arr, target):
+#     for i in range(len(arr)):
+#         if arr[i] == target:
+#             return i
+#     else:
+#         return 'Not found'
+    
+# arr = [5, 8, 2, 8, 10]
+# target = 8
+# print(occurence(arr, target))
+
+
+# Count how many times a target appears.
+
+# def appearance(arr, target):
+#     for i in range(len(arr)):
+#         return arr.count(target)
+
+# arr = [1,2,3,2,4,2,5]
+# target = 2
+# print(appearance(arr, target))
+
+
+# Find the largest number using linear search.
+
+# def largest(arr):
+#     for i in range(len(arr)):
+#         return max(arr)
+    
+# arr = [12,5,98,44,21]
+# print(largest(arr))
+
